d268c578e7cd94f19c703fd2f50db13516dd6cad
giahann112005 <10623012@student.vgu.edu.vn>
2024-06-27 13:14:29 +0700

    Update web.py

--- web.py
+++ web.py
@@ -55,10 +55,10 @@ with tab2:
 with tab3:
     average_scores = sp.groupby('parental_level_of_education')[['math_score', 'reading_score', 'writing_score']].mean().reset_index()
     average_scores['average_score'] = average_scores[['math_score', 'reading_score', 'writing_score']].mean(axis=1)
-    st.header('Top Levels Of Parental Education That Correspond With The Highest Average Score.')
+    st.header("Top Levels Of Parental Education That Correspond With The Highest Average Score")
     top_n = st.slider('Select number of groups', 1, len(average_scores), 1)
     top_groups = average_scores.nlargest(top_n, 'average_score')
-    fig = px.bar(top_groups, x='parental_level_of_education', y='average_score', title=f"Top {top_n} Groups That Have Highest Average Score")
+    fig = px.bar(top_groups, x='parental_level_of_education', y='average_score', title=f"Top {top_n} Levels Of Parental Education That Correspond With The Highest Average Score")
     st.plotly_chart(fig)
 with tab1:
     st.sidebar.header('Filter Options For Scatter Chart')
